72c658ade5018ec16d653dd9938a73d9c275fec6
Andrea-Rivera <36813115+Andrea-Rivera@users.noreply.github.com>
2023-02-22 16:51:56 +1000

    Solving HTTPS Problem

--- src/Components/RecipeInfo.jsx
+++ src/Components/RecipeInfo.jsx
@@ -13,7 +13,7 @@ function RecipeInfo() {
     
 
     if(MealId!==""){
-        fetch(`https:/www.themealdb.com/api/json/v1/1/lookup.php?i=${MealId}`)
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${MealId}`)
         .then(res=>res.json())
         .then(data=>{
             setItem(data.meals[0]);
